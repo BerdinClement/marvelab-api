dca96cbe6eff45cff8071468fbddcbc317729c39
Clément Berdin <clement.berdin@gmail.com>
2025-06-23 10:30:45 +0200

    feat(notes): init notes module

---
+++ src/notes/notes.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Body, Patch, Param } from '@nestjs/common';
+import { NotesService } from './notes.service';
+import { UpdateNoteDto } from './dto/update-note.dto';
+
+@Controller('notes')
+export class NotesController {
+  constructor(private readonly notesService: NotesService) {}
+
+  @Get()
+  findAll() {
+    return this.notesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.notesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateNoteDto: UpdateNoteDto) {
+    return this.notesService.update(id, updateNoteDto);
+  }
+
+}
---
+++ src/notes/notes.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { UpdateNoteDto } from './dto/update-note.dto';
+import { Note } from './entities/note.entity';
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+
+@Injectable()
+export class NotesService {
+  private readonly notes: Note[];
+
+  constructor() {
+    const filePath = path.resolve(__dirname, '../../db/data.json');
+    const rawData = fs.readFileSync(filePath, 'utf8');
+    const parsedData = JSON.parse(rawData) as { notes: Note[] };
+    this.notes = parsedData.notes;
+  }
+  findAll() {
+    return this.notes;
+  }
+
+  findOne(id: number) {
+    const note = this.notes.find((note) => note.id === id);
+    if (!note) {
+      throw new Error(`Note with ID ${id} not found`);
+    }
+    return note;
+  }
+
+  update(id: number, updateNoteDto: UpdateNoteDto) {
+    const noteIndex = this.notes.findIndex((note) => note.id === id);
+    if (noteIndex === -1) {
+      throw new Error(`Note with ID ${id} not found`);
+    }
+
+    const updatedNote: Note = {
+      ...this.notes[noteIndex],
+      ...updateNoteDto,
+    };
+
+    this.notes[noteIndex] = updatedNote;
+
+    const filePath = path.resolve(__dirname, '../../db/data.json');
+    fs.writeFileSync(filePath, JSON.stringify({ notes: this.notes }, null, 2));
+
+    return updatedNote;
+  }
+}
